5b4746608e9e10ce5396a2e58d91535ff6a54d2b
Rizky Ramadhan <ramarizdev@gmail.com>
2024-03-27 11:34:02 +0700

    feat(exercises): add exercise1 and exercise2 solution

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,18 +1,32 @@
 package com.adepuu.exercises.session6;
 
+import java.util.Scanner;
+
 public class Exercise1 {
-    /**
-     * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
-     *
-     * Input: 1, 2, 3, 4, 5, q
-     * Output: 3
-     *
-     * Input: 1, 2, 3, a, b, 4, 5, q
-     * Output: 3
-     *
-     * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
-     */
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isContinue = true;
+        double sum = 0;
+        int i = 0;
+
+        while(isContinue) {
+            System.out.print("Enter a number : ");
+            String input = scanner.next();
+
+            if(input.equals("q") || input.equals("Q")) {
+                isContinue = false;
+            } else {
+                try {
+                    sum += Double.parseDouble(input);
+                    i++;
+                } catch(Exception e) {
+                    System.out.println("Enter a valid number");
+                }
+            }
+        }
+
+        System.out.println(sum / i);
 
     }
 }
--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,7 +1,10 @@
 package com.adepuu.exercises.session6;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercise2 {
     /**
@@ -18,10 +21,23 @@ public class Exercise2 {
 
     static void readFileContents(String fileName) {
         BufferedReader reader = null;
-        reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch(IOException e) {
+            System.out.println("Something went wrong");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
     }
 }
